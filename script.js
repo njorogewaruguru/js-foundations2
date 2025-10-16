8af8cc6a3b9b732dae6d2e52e58c1c4db49a4872
njorogewaruguru <warugurunjoroge8@gmail.com>
2025-10-16 13:38:42 +0300

    Add second question

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
-// function groceryList() {
-//     let groceries = ['kales', 'bread', 'milk', 'sugar']
-//     for (let i = 0; i <= groceries.length-1; i++) {
-//         console.log(`${i +1}.${groceries[i]}`)
+// function groceryList(groceries) {
+
+//     for (let i = 0; i <= groceries.length - 1; i++) {
+//         console.log(`${i + 1}.${groceries[i]}`)
 //     }
 // }
+// let groceries = ['kales', 'bread', 'milk', 'sugar']
+// groceryList(groceries)
 
-// groceryList()
 
 
-
+function showMarks(details) {
+    for (let i = 0; i < details.length; i++) {
+        let student = details[i];
+        let total = 0;
+        console.log(`\nName: ${student.studentName}`);
+        console.log("Subjects and Marks")
+        for (let j = 0; j < student.marks.length; j++) {
+            let subject = student.marks[j]
+            console.log(`${subject.subject}: ${subject.score}`)
+            total += subject.score;
+        }
+        let average = total / student.marks.length;
+        let status;
+        if(average >= 50){
+            status = "Pass"
+        } else {
+            status = "Fail"
+        }
+        console.log(`Average: ${average.toFixed(2)}`)
+        console.log(`Result: ${status}`);
+    }
+}
 
 let details = [
     {
-        studentName: 'hannah',
+        studentName: 'Hannah',
 
-        subjects: [{ math: 95 }, { language: 90 }, { "science": 80 }, { "comp": 90 }]
+        marks: [{ subject: "Math", score: 95 }, { subject: "Language", score: 90 }, { subject: "Science", score: 80 }, { subject: "Comp", score: 90 }]
+    },
+    {
+        studentName: 'Mary',
+
+        marks: [{ subject: "Math", score: 90 }, { subject: "Language", score: 70 }, { subject: "Science", score: 68 }, { subject: "Comp", score: 57 }]
+    },
+    {
+        studentName: 'James',
+
+        marks: [{ subject: "Math", score: 64 }, { subject: "Language", score : 79 }, { subject: "Science", score: 78 }, { subject: "Comp", score: 49 }]
     },
 
+    {
+        studentName: 'Ben',
+
+        marks: [{ subject: "Math", score: 46 }, { subject: "Language", score : 30 }, { subject: "Science", score: 28 }, { subject: "Comp", score: 49 }]
+    }
+
 ]
+showMarks(details)
