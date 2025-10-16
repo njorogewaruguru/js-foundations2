c549be7da11ce53ddbdead62f201f8359f127aef
njorogewaruguru <warugurunjoroge8@gmail.com>
2025-10-16 11:35:30 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// function groceryList() {
+//     let groceries = ['kales', 'bread', 'milk', 'sugar']
+//     for (let i = 0; i <= groceries.length-1; i++) {
+//         console.log(`${i +1}.${groceries[i]}`)
+//     }
+// }
+
+// groceryList()
+
+
+
+
+let details = [
+    {
+        studentName: 'hannah',
+
+        subjects: [{ math: 95 }, { language: 90 }, { "science": 80 }, { "comp": 90 }]
+    },
+
+]
